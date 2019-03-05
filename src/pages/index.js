234b672f3f9512337ee29665bb19af7b90440d57
Thomas Johnson <tjohnson1106@gmail.com>
2019-03-04 19:52:31 -0500

    created card styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,32 @@
 import React from "react";
 import { withRouteData } from "react-static";
+import { Link } from "@reach/router";
 
-export default withRouteData(
-  ({ posts }) =>
-    console.log({ posts }) || (
-      <div style={{ textAlign: "center" }}>
-        <h1>Welcome to React-Static</h1>
-      </div>
-    )
-);
+export default withRouteData(({ posts }) => (
+  <div className="container">
+    {posts.map((post) => (
+      <Link key={post.id} to={`/post/${post.id}`} className="card">
+        <div>
+          <img
+            alt={post.title}
+            className="card-img"
+            src={`https://media.graphcms.com/resize=w:170,h:150,fit:crop/${
+              post.image.handle
+            }`}
+          />
+        </div>
+
+        <div>
+          <h3>{post.title}</h3>
+          <p>{post.author.name}</p>
+
+          {post.tags.map((tag) => (
+            <span className="card-tag" key={tag}>
+              {tag}
+            </span>
+          ))}
+        </div>
+      </Link>
+    ))}
+  </div>
+));
