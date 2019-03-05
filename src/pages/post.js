03e0dc5635e394cd7db7166ff3bef53a6179332c
Thomas Johnson <tjohnson1106@gmail.com>
2019-03-04 20:28:46 -0500

    added react markdown library

--- src/pages/post.js
+++ src/pages/post.js
@@ -1,4 +1,19 @@
 import React from "react";
+import Markdown from "react-markdown";
 import { withRouteData } from "react-static";
 
-export default withRouteData(({ post }) => <div>{JSON.stringify(post)}</div>);
+export default withRouteData(({ post }) => (
+  <article>
+    <h1>{post.title}</h1>
+    <div className="placeholder">
+      <img
+        alt={post.title}
+        src={`https://media.graphcms.com/resize=w:650,h:366,fit:crop/${
+          post.image.handle
+        }`}
+      />
+    </div>
+
+    <Markdown source={post.content} />
+  </article>
+));
