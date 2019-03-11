407fcb8497ecd4e154dce7d9644c6c698799f0d5
Thomas Johnson <tjohnson1106@gmail.com>
2019-03-11 18:41:47 -0400

    created active underline function

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,14 +2,22 @@ import React from "react";
 import { withSiteData } from "react-static";
 import { Link } from "@reach/router";
 
+const isActive = ({ isCurrent }) => {
+  return isCurrent
+    ? { className: "active" }
+    : {
+        className: "header-link"
+      };
+};
+
 export default withSiteData(({ title }) => (
   <header className="header">
     <h1 className="header-h1">{title}</h1>
     <nav className="header-name">
-      <Link className="header-link" to="/">
+      <Link getProps={isActive} className="header-link" to="/">
         Home
       </Link>
-      <Link className="header-link" to="/about">
+      <Link getProps={isActive} className="header-link" to="/about">
         About
       </Link>
     </nav>
