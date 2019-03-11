2904603993877dd1e15fce67dc9f1f40d3027086
Thomas Johnson <tjohnson1106@gmail.com>
2019-03-11 16:09:29 -0400

    implemented about page data

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,24 @@
 import React from "react";
 import { withRouteData } from "react-static";
 
-export default withRouteData(
-  ({ authors }) =>
-    console.log({ authors }) || (
-      <div>
-        <p>React Static is a progressive static site generator for React.</p>
+export default withRouteData(({ authors }) => (
+  <div className="">
+    <h1 className="about-header">About the Authors</h1>
+    {authors.map((author) => (
+      <div className="about-author" key={author.id}>
+        <div className="about-header">
+          <img
+            className="about-avatar"
+            alt={author.name}
+            src={`https://media.graphcms.com/resize=w:100,h:100,fit:crop/${
+              author.avatar.handle
+            }`}
+          />
+          <h2>By {author.name}</h2>
+        </div>
+
+        <p>{author.bibliography}</p>
       </div>
-    )
-);
+    ))}
+  </div>
+));
